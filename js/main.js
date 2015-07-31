6122ebb5305ca309fe0b1fbe87136a692d2f0f22
amerriman <amm434@gmail.com>
2015-07-31 11:56:45 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// add scripts
+
+$(document).on('ready', function() {
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    var toDo = $('input').val();
+    $('#all-todos').prepend('<li><h2>'+toDo+
+      '&nbsp;<button class="btn btn-danger btn-sm">X</button></h2></li>');
+  });
+
+
+});
